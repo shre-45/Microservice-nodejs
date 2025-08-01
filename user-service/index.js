5258d30eec4e2ac4010d15a995e6ffd63a464002
SHRESHTA ANUGU <131268085+shre-45@users.noreply.github.com>
2025-08-01 14:39:41 +0530

    Update index.js

--- user-service/index.js
+++ user-service/index.js
@@ -17,4 +17,4 @@ app.post("/login", (req, res) => {
   res.json({ token: Math.random().toString(36).substring(2) });
 });
 
-app.listen(3000, () => console.log("User Service on 3001"));
+app.listen(3001, () => console.log("User Service on 3001"));
